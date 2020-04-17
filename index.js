5b554202d8e914beedc0f31852b63fc18619e1c6
DonaldihnoTrumporino <mtkowpak@hotmail.com>
2020-04-17 17:14:14 -0400

    update

--- index.js
+++ index.js
@@ -188,8 +188,8 @@ client.on("message", async message => {
   // react with an emoji
 
   if ((message.author.id.toString() == KowpakID)) {
-    const emoji = client.emojis.find(emoji => emoji.name === "mike");
-    message.react(emoji.id);
+    //const emoji = client.emojis.cache.find(emoji => emoji.name === "mike");
+    message.react(emoji);
   }
 
 
